66249378c486ea5627db822daab022fc0fcaaa75
Marco Hernandez <marco7245@hotmail.com>
2023-03-18 22:53:08 -0600

    Peticion a API Exitosa

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -5,10 +5,9 @@ export const getPokemons = (page = 0)=>{
     return async( dispatch,getState ) =>{
         dispatch(startLoadingPokemons());
 
-        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151&offset=${ page*10}`);
-        // const data = await resp.json();
-        const {data} = await pokemonApi.get(`/pokemon?limit=151&offset=${ page*10}`);
+        // TODO: realizar Peticion http 
+        const {data} = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10}`);
 
-        dispatch(setPokemons({pokemons:data.results,page:page+1}))
+        dispatch(setPokemons({pokemons:data.results,page: page + 1}))
     }
 }
